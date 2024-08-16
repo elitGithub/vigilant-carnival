b1d2488e4b67d11a5bf6f90f09ffcab3ba891928
Eli <akanoou@gmail.com>
2024-08-16 23:38:47 +0300

    change to any route

--- routes/web.php
+++ routes/web.php
@@ -3,4 +3,4 @@
 use App\Http\Controllers\NadlanApiController;
 use Illuminate\Support\Facades\Route;
 
-Route::post('/api/assets-deals', [NadlanApiController::class, 'apiGetAssetsAndDeals']);
+Route::any('/api/assets-deals', [NadlanApiController::class, 'apiGetAssetsAndDeals']);
