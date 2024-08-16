17390ae2d37a6830f617cf0156d5b336b8bcf3b8
Eli <akanoou@gmail.com>
2024-08-17 00:27:39 +0300

    re-reoute

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
+use App\Http\Controllers\NadlanApiController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::prefix('api')->group(function () {
+   Route::any('/assets-deals', [NadlanApiController::class, 'apiGetDataByQuery']);
 });
