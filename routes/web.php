bc7746de4d7ffc3cdcbc01d0f24610ecbf0596a7
Eli <akanoou@gmail.com>
2024-08-17 01:37:03 +0300

    add cache clearing

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,17 @@
 <?php
 
-use App\Http\Controllers\NadlanApiController;
+use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/cache-clear', function () {
+    Artisan::call('config:cache');
+    Artisan::call('cache:clear');
+    Artisan::call('route:clear');
+    Artisan::call('route:cache');
     return view('welcome');
 });
 
