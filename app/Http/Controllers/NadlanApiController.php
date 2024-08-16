f541da78c88c9ceb1a37688c62040b132a3f213b
Eli <akanoou@gmail.com>
2024-08-17 01:19:11 +0300

    add get assets and deals method

--- app/Http/Controllers/NadlanApiController.php
+++ app/Http/Controllers/NadlanApiController.php
@@ -23,7 +23,7 @@ class NadlanApiController extends Controller
 
         try {
             $response = Http::withHeaders([
-                'Content-Type' => 'application/json'
+                'Content-Type' => 'application/json',
             ])->withUserAgent("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36")
                             ->timeout(30)
                             ->withoutVerifying()  // Disables SSL certificate verification
@@ -39,6 +39,43 @@ class NadlanApiController extends Controller
         } catch (\Exception $e) {
             Log::error("Error in GetApiDataByQuery: " . $e->getMessage());
             return response()->json(['error' => $e->getMessage(), 'trace' => $e->getTrace(), 'request_url' => $url], 500);
+        }
+    }
+
+    /**
+     * @param  \Illuminate\Http\Request  $request
+     *
+     * @return \Illuminate\Http\JsonResponse
+     * @throws \Illuminate\Http\Client\ConnectionException
+     */
+    public function apiGetAssetsAndDeals(Request $request): JsonResponse
+    {
+        // Extract data from the request
+        $count = $request->input('count');
+        $result = $request->input('result');
+        $city = $request->input('city');
+        $shuna = $request->input('shuna');
+
+        // Clean and prepare data
+        $shuna = addslashes(str_replace('"', '', $shuna));
+        $resulttemp = str_replace('"PageNo":0', '"PageNo":' . $count, $result);
+
+        // Perform the HTTP request using Laravel's HTTP client
+        $response = Http::withHeaders([
+            'Content-Type'  => 'application/json',
+            'cache-control' => 'no-cache',
+        ])->withOptions([
+            'verify' => false,
+        ])->post('https://www.nadlan.gov.il/Nadlan.REST/Main/GetAssestAndDeals', $resulttemp);
+
+        // Check if the request was successful
+        if ($response->successful()) {
+            $data = $response->json();
+            return response()->json($data);
+        } else {
+            // Log and handle errors
+            Log::error("Failed to retrieve data from API for city: {$city}, shuna: {$shuna}");
+            return response()->json(['error' => 'Failed to retrieve data from the API'], 400);
         }
     }
 
--- routes/web.php
+++ routes/web.php
@@ -8,5 +8,6 @@ Route::get('/', function () {
 });
 
 Route::prefix('api')->group(function () {
-   Route::any('/assets-deals', [NadlanApiController::class, 'apiGetDataByQuery']);
+   Route::any('/assets-deals', [NadlanApiController::class, 'apiGetAssetsAndDeals']);
+   Route::any('/get-data-by-query', [NadlanApiController::class, 'apiGetDataByQuery']);
 });
