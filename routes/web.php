9f3d1e8c42ac63a39bed47fe6ea1dd86cc8da461
Eli <akanoou@gmail.com>
2024-08-17 00:23:30 +0300

    uninstall and reinstall

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
-use App\Http\Controllers\NadlanApiController;
 use Illuminate\Support\Facades\Route;
 
-Route::any('/api/assets-deals', [NadlanApiController::class, 'apiGetDataByQuery']);
+Route::get('/', function () {
+    return view('welcome');
+});
