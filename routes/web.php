771e067bcdd3f4ce655565dec23b1c01546abe31
Eli <akanoou@gmail.com>
2024-08-20 00:00:50 +0300

    maybe like that

--- routes/web.php
+++ routes/web.php
@@ -8,10 +8,10 @@ Route::get('/', function () {
 });
 
 Route::get('/cache-clear', function () {
+    Artisan::call('cache:clear');
     Artisan::call('make:cache-table');
     Artisan::call('migrate');
     Artisan::call('config:cache');
-    Artisan::call('cache:clear');
     Artisan::call('route:clear');
     Artisan::call('route:cache');
     return view('welcome');
