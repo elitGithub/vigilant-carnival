58d9de502b73c51f3c935c838cf27e27ebda8e4b
Eli <akanoou@gmail.com>
2024-08-20 00:25:35 +0300

    add api back

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,14 @@
 <?php
 
-use App\Http\Controllers\NadlanApiController;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+
+use App\Http\Controllers\NadlanApiController;
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
+
 
 Route::prefix('api')->group(function () {
     Route::any('/assets-deals', [NadlanApiController::class, 'apiGetAssetsAndDeals']);
